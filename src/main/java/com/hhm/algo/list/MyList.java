33d0f8a8f397a4d9c600e8f9e5db38473d126af4
minhaihuang <1533050609@qq.com>
2019-06-28 09:05:35 +0800

    完善list的代码

--- src/main/java/com/hhm/algo/list/MyList.java
+++ src/main/java/com/hhm/algo/list/MyList.java
@@ -1,5 +1,8 @@
 package com.hhm.algo.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 链表
  * 时间复杂度
@@ -32,6 +35,47 @@ public class MyList {
             insertNode.next = nextNode;
         }
         size++;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    /**
+     * 删除节点
+     * @param index
+     * @return
+     */
+    public Node remove(int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("超出链表节点范围");
+        }
+        Node removeNode = null;
+        // 头部删除
+        if(index == 0){
+            removeNode = head;
+            head = head.next;
+        } else if(index == size-1){  // 尾部删除
+            Node tempNode = getNode(index - 1);
+            removeNode = tempNode.next;
+            tempNode.next = null;
+            last = tempNode;
+        } else { //中间删除
+            Node tempNode = getNode(index - 1);
+            Node nextNode = tempNode.next.next;
+            removeNode = tempNode.next;
+            tempNode.next = nextNode;
+        }
+        return removeNode;
+    }
+
+    public void output(){
+        if(head != null){
+           while (head != null) {
+               System.out.println(head.getData());
+               head = head.next;
+           }
+        }
     }
 
     public Node getNode(Integer index){
@@ -73,5 +117,19 @@ public class MyList {
         public void setNext(Node next) {
             this.next = next;
         }
+    }
+
+    public static void main(String[] args) {
+        MyList myList = new MyList();
+        myList.insert(3,0);
+        myList.insert(5,1);
+        myList.insert(6,2);
+        myList.insert(4,3);
+        myList.insert(2,4);
+        myList.insert(9,5);
+
+        System.out.println(myList.size);
+        System.out.println("===================");
+        myList.output();
     }
 }
